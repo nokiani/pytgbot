d4ec706278717a2436b166b910dbe39fe37d9baf
Alexey Zolotarev <alexey.zolotareff@gmail.com>
2024-03-26 19:22:01 +0500

    First Telegram Aiogram class

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pytgbot
+
+Телеграм бот на aiogram
+https://youtube.com/playlist?list=PLYnH8mpFQ4am8cFYqn2KsPLb-HrhcYgtC&si=5rWSva3fwWmWmtCv
---
+++ tg_aiogram_bot.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+from aiogram.filters import CommandStart, Command
+
+import tg_config
+
+bot = Bot(token=tg_config.BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def handle_start(message: types.Message):
+  await message.answer(text=f"Hello? {message.from_user.full_name}!")
+
+@dp.message(Command("help"))
+async def handle_help(message: types.Message):
+  text = "I'm ECHO bot.\nSend me any message!"
+  await message.answer(text=text)
+
+@dp.message()
+async def echo_message(message: types.Message):
+  await message.answer(text="Wait a second...")
+
+  try:
+    await message.send_copy(chat_id=message.chat.id)
+  except TypeError:
+    await message.reply(text="Oh! Something new :)")
+
+async def main():
+  logging.basicConfig(level=logging.INFO)
+  await dp.start_polling(bot)
+
+if __name__ == "__main__":
+  asyncio.run(main())
